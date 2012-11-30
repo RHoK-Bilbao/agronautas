4f1651a7ffe580528821a3f9ed1bdfbdc00726ef
Aitor Almeida <aitoralmeida@gmail.com>
2012-11-30 16:35:16 +0100

    changed client to be able to generate a dataset

--- software/lod/client.py
+++ software/lod/client.py
@@ -47,18 +47,23 @@ elif createDataset:
         # Connect to server and send data
         sock.connect((HOST, PORT))
         sock.sendall("INSERT@43.29564@-2.99729@supercocina@2100000000@4@arroz\n")
-        sock.sendall("INSERT@43.29564@-2.99729@supercocina@3100000000@4@verduras\n")
-        sock.sendall("INSERT@43.29564@-2.99729@micrococina@5100000000@4 13:20:12@4@verduras\n")
-        sock.sendall("UPDATE@supercocina@134@23.0@70.3@3100000000@4 12:25:13")
-        sock.sendall("UPDATE@supercocina@14@13.0@72.3@2100000010")
-        sock.sendall("UPDATE@micrococina@134@23.0@70.3@5100300000@4")
-        sock.sendall("UPDATE@micrococina@114@24.0@60.3@5100500000@4")
-    
-        # Receive data from the server and shut down
-        received = sock.recv(2048)
     finally:
         sock.close()
-    
+    try:
+        # Connect to server and send data
+        sock.connect((HOST, PORT))
+        sock.sendall("INSERT@43.29564@-2.99729@supercocina@3100000000@4@verduras\n")
+    finally:
+        sock.close()
+        
+        """
+         
+        sock.sendall("INSERT@43.29564@-2.99729@micrococina@5100000000@4@verduras\n")
+        sock.sendall("UPDATE@supercocina@134@23.0@70.3@3100000000")
+        sock.sendall("UPDATE@supercocina@14@13.0@72.3@2100000010")
+        sock.sendall("UPDATE@micrococina@134@23.0@70.3@5100300000")
+        sock.sendall("UPDATE@micrococina@114@24.0@60.3@5100500000")
+    """
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
