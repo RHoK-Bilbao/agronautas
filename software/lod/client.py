55b04510b5f493c708f725c2b2318b4a7ab99f22
Aitor Almeida <aitoralmeida@gmail.com>
2012-11-30 12:19:55 +0100

    changes to move the development to mikel's comp

--- software/lod/client.py
+++ software/lod/client.py
@@ -8,7 +8,8 @@ Created on Thu Nov 22 10:33:44 2012
 import socket
 
 HOST, PORT = "localhost", 9999
-data = "43.29564@-2.99729@nameBlah@lumiBlah@exterBalh@interblah"
+data = "INSERT@43.29564@-2.99729@nameBlah"
+# data = "INSERT@43.29564@-2.99729@nameBlah@lumiBlah@exterBalh@interblah"
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- software/lod/server.py
+++ software/lod/server.py
@@ -9,6 +9,7 @@ import SocketServer
 import re
 import urllib
 from xml.dom import minidom
+from rdflib.graph import Graph
 
 """
 Expected msg format: "lat@long@name@luminosity@externalTemp@internalTemp"
@@ -19,8 +20,9 @@ verbose = True
 HOST = "localhost"
 PORT = 9999
 
-# RDF template                                           
-rdfTemplateCooker = """
+# RDF template    
+# TODO meter cooker id como URL                                       
+rdfTemplateCookerInsert = """
 <?xml version="1.0"?>
                     
 <rdf:RDF
@@ -54,10 +56,42 @@ xml:base="http://www.morelab.deusto.es/agronautasSimple.owl">
 </rdf:RDF>
 """
 
+rdfTemplateCookerUpdate = """
+<?xml version="1.0"?>
+                    
+<rdf:RDF
+xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+xmlns:protege="http://protege.stanford.edu/plugins/owl/protege#"
+xmlns:xsp="http://www.owl-ontologies.com/2005/08/07/xsp.owl#"
+xmlns:j.0="http://xmlns.com/foaf/0.1/"
+xmlns:owl="http://www.w3.org/2002/07/owl#"
+xmlns:j.1="http://geovocab.org/spatial#"
+xmlns:xsd="http://www.w3.org/2001/XMLSchema#"
+xmlns:swrl="http://www.w3.org/2003/11/swrl#"
+xmlns:swrlb="http://www.w3.org/2003/11/swrlb#"
+xmlns="http://www.morelab.deusto.es/agronautasSimple.owl#"
+xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+xml:base="http://www.morelab.deusto.es/agronautasSimple.owl">
+
+<SolarCooker rdf:ID="%(COOKER_ID_HOLDER)s">
+    <name rdf:datatype="http://www.w3.org/2001/XMLSchema#string">%(NAME_HOLDER)s</name>
+    <hasLocation>
+        <Location rdf:ID="%(LOCATION_ID_HOLDER)s">
+            <latitude rdf:datatype="http://www.w3.org/2001/XMLSchema#float">%(LATITUDE_HOLDER)s</latitude>
+            <longitude rdf:datatype="http://www.w3.org/2001/XMLSchema#float">%(LONGITUDE_HOLDER)s</longitude>
+            <nearTo rdf:resource="http://www.geonames.org/%(NEAR_TO_HOLDER)s/about.rdf"/>
+        </Location>
+    </hasLocation>
+</SolarCooker>
+                        
+</rdf:RDF>
+"""
+
+"""
 def convertToRDF(data):
     
         # get the data
-        latitude, longitude, name, luminosity, tempExternal, tempInternal = re.split("@", data)
+        command, latitude, longitude, name, luminosity, tempExternal, tempInternal = re.split("@", data)
         
         nearplace = getNearerPlace(latitude,longitude)
                 
@@ -80,7 +114,7 @@ def convertToRDF(data):
             print "************END**************"
             
         return rdfStrCooker  
-        
+"""        
         
 def getNearerPlace(lat, lng):
        
@@ -92,6 +126,35 @@ def getNearerPlace(lat, lng):
 
     return itemlist[len(itemlist)-1].childNodes[0].data
     
+def insertCooker(data):
+    command, latitude, longitude, name = re.split("@", data)
+    
+    nearplace = getNearerPlace(latitude,longitude)
+                
+    # complete the RDF        
+    rdfStrCooker = rdfTemplateCookerInsert % {
+        "LOCATION_ID_HOLDER" : "location" + name,
+        "LATITUDE_HOLDER" : latitude,
+        "LONGITUDE_HOLDER" : longitude,
+        "NEAR_TO_HOLDER" : nearplace, 
+        "COOKER_ID_HOLDER" : "cooker" + name,
+        "NAME_HOLDER" : name
+    }
+                           
+    if verbose:
+        print "************RESULT**************"
+        print rdfStrCooker
+        print "************END**************"
+        
+    g = Graph()
+    g.parse(rdfStrCooker)
+    
+    print len(g)
+    
+    return rdfStrCooker  
+    
+def updateData(data):
+    return 'bla'
 
 
 class MyTCPHandler(SocketServer.BaseRequestHandler): 
@@ -99,16 +162,21 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
      def handle(self):
         self.data = self.request.recv(1024).strip()
         
+        if self.data.startswith == 'INSERT':
+            insertCooker(self.data)        
+        elif self.data.startswith == 'UPDATE':
+            updateData(self.data)
+        
         if verbose:
             import threading
             print threading.currentThread()
             print "{} wrote:".format(self.client_address[0])
             print self.data
         
-        rdf = convertToRDF(self.data)        
+       # rdf = convertToRDF(self.data)        
         
         # TODO: send the rdf info to virtuoso
-        self.request.sendall(rdf)    
+        # self.request.sendall(rdf)    
 
 
 if __name__ == "__main__":
