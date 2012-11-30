f4653719f8a305814a59faf3a4acd305ffaabb10
Aitor Almeida <aitoralmeida@gmail.com>
2012-11-30 16:03:11 +0100

    we don't need the registry commmand

--- software/lod/client.py
+++ software/lod/client.py
@@ -8,22 +8,23 @@ Created on Thu Nov 22 10:33:44 2012
 import socket
 
 insert = True
-register = False
+#register = False
 update = False
 
 HOST, PORT = "localhost", 9999
 
-dataRegister = "REGISTER@nombrecocina"
+#dataRegister = "REGISTER@nombrecocina"
 dataInsert = "INSERT@43.29564@-2.99729@supercocina@2012-11-30@4@arroz"
 dataUpdate = "UPDATE@supercocina@134@23.0@70.3@2012-11-30"
-# data = "INSERT@43.29564@-2.99729@nameBlah@lumiBlah@exterBalh@interblah"
 
 if insert:
     data = dataInsert
 elif update:
     data = dataUpdate
-elif insert:
-    data = dataRegister        
+"""
+elif register:
+    data = dataRegister   
+"""     
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
