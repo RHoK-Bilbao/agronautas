6f655c2c4aa8f57a1f853a7991130ac6e87a8ae9
s3r4v0k <s3r4v0k@Juan-Netbook.(none)>
2012-11-30 16:22:20 +0100

    Añadido led +  SD + log

--- hardware/agrococinas_sw/agrococinas_sw.ino
+++ hardware/agrococinas_sw/agrococinas_sw.ino
@@ -1,14 +1,20 @@
 #include <EEPROM.h>
+#include <SdFat.h>
+SdFat sd;
+SdFile myFile;
 //declare variables
+const int chipSelect = 10;
 float tempInC;
 float humedC;
 float luminC;
 float tempOutC;
+char* luminL;
 
 int tempInPin = A0;
 int humedPin = A1;
 int luminPin = A2;
 int tempOutPin = A3;
+int led = 7;
 int samples[8]; // 8 samples to have a large sample
 
 int addr = 0;
@@ -17,15 +23,90 @@ int addr2 = 0;
 int addr3 = 0;
 
 float supplyVolt = 5.0;
+int temp_warning = 22;
 char* msg = "";
 
 
 void setup()
 {
   Serial.begin(9600); //opens serial port, sets data rate to 9600 bps
+   pinMode(led, OUTPUT);   
   // write a 0 to all 512 bytes of the EEPROM
   for (int i = 0; i < 512; i++)
     EEPROM.write(i, 0);
+    
+}
+
+void writeSD(){
+if (!sd.begin(chipSelect, SPI_HALF_SPEED)) sd.initErrorHalt();
+
+  // open the file for write at end like the Native SD library
+  if (!myFile.open("log.txt", O_RDWR | O_CREAT | O_AT_END)) {
+    sd.errorHalt("opening test.txt for write failed");
+  }
+
+
+  myFile.print("Temperatura Externa: ");
+  myFile.print(tempOutC,0); 
+  myFile.println();
+  myFile.print("Luminosidad: ");
+  myFile.print(luminC, 0); 
+  myFile.print("  ");
+  myFile.print(luminL);
+  myFile.println();
+  myFile.print("Humedad: ");
+  myFile.print(humedC,0);
+  myFile.println();
+  myFile.print("Temperatura Interna: ");
+  myFile.print(tempInC,0);  
+  myFile.println();
+
+  // close the file:
+  myFile.close();
+  Serial.println("done.");
+
+}
+
+void readSD(){
+  if (!sd.begin(chipSelect, SPI_HALF_SPEED)) sd.initErrorHalt();
+  // re-open the file for reading:
+  if (!myFile.open("log.txt", O_READ)) {
+    sd.errorHalt("opening test.txt for read failed");
+  }
+  Serial.println("log.txt:");
+
+  // read from the file until there's nothing else in it:
+  int data;
+  while ((data = myFile.read()) > 0) Serial.write(data);
+  // close the file:
+  myFile.close();
+}
+
+void evaluate_temp()
+{
+  Serial.println(tempOutC);
+  Serial.println(temp_warning);
+  if(tempOutC> temp_warning){
+    Serial.print("Temperatura superior a ");
+    Serial.println(temp_warning);
+    digitalWrite(led, HIGH); 
+  }else{
+     digitalWrite(led, LOW); 
+  }
+}
+
+void evaluate_lumi()
+{
+  if(luminC < 75){
+    luminL = "Muy baja";
+  }else if(luminC < 150){
+    luminL = "baja";
+  }else if(luminC < 225){
+    luminL = "luminoso";
+  }else{
+    luminL = "muy luminoso";
+  }
+  
 }
 void loop()
 {
@@ -35,6 +116,7 @@ void loop()
   delay(20);
   tempOutC = analogRead(tempOutPin);           //read the value from the sensor
   tempOutC = (supplyVolt * tempOutC * 100.0)/1023.0;  //convert the analog data to temperature
+  evaluate_temp();
   delay(20);
   humedC = analogRead(humedPin);           //read the value from the sensor
   float voltage = humedC/1023. * supplyVolt; // convert to voltage value
@@ -42,7 +124,9 @@ void loop()
   //humedC = ((((humedC/1023)*5)-0.8)/3.1)*100;  //convert the analog data to HR%
   humedC = (sensorRH)/(1.0546-0.00216*(tempOutC)); // temperature compensation
   delay(20);
+
   luminC = analogRead(luminPin);           //read the value from the sensor
+  evaluate_lumi();
   //luminC = (5.0 * luminC * 100.0)/1024.0;  //convert the analog data 
   Serial.println("=====================");
   Serial.print("Temperatura Externa: ");
@@ -50,6 +134,8 @@ void loop()
   Serial.println();
   Serial.print("Luminosidad: ");
   Serial.print(luminC, 0); 
+  Serial.print("  ");
+  Serial.print(luminL);
   Serial.println();
   Serial.print("Humedad: ");
   Serial.print(humedC,0);
@@ -57,8 +143,8 @@ void loop()
   Serial.print("Temperatura Interna: ");
   Serial.print(tempInC,0);  
   Serial.println();
-  
-  msg = "INSERT@"+43.27123+"@"+-2.93855 +"@"+ProtoCocina+"@"+timestamp
+  writeSD();
+  msg = "INSERT@43.27123@-2.93855@ProtoCocina@";
   Serial.println(msg);
   /*
 Serial.print((byte)humed1C ); 
